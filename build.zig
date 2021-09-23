e1564c97f0379c60d690cac77b09ed36cf569527
Ryan Liptak <squeek502@hotmail.com>
2021-09-23 03:34:11 -0700

    Update to build-lib method instead of build-obj
    See:
    https://github.com/squeek502/zig-fuzzing-example/commit/7a13c2cb6625f51a6872a659d4656519bf96bdae
    https://github.com/squeek502/ryanliptak.com/commit/bcc6785a859d0a11b3a4f5dacd1a755099436c6c

--- build.zig
+++ build.zig
@@ -7,21 +7,20 @@ pub fn build(b: *std.build.Builder) !void {
 }
 
 fn addFuzzer(b: *std.build.Builder, comptime name: []const u8) !void {
-    // The object file
-    const fuzz_obj = b.addObject("fuzz-" ++ name ++ "-obj", "fuzzers/" ++ name ++ ".zig");
-    fuzz_obj.setBuildMode(.Debug);
-    fuzz_obj.want_lto = true;
+    // The library
+    const fuzz_lib = b.addStaticLibrary("fuzz-" ++ name ++ "-lib", "fuzzers/" ++ name ++ ".zig");
+    fuzz_lib.setBuildMode(.Debug);
+    fuzz_lib.want_lto = true;
+    fuzz_lib.bundle_compiler_rt = true;
 
     // Setup the output name
     const fuzz_executable_name = "fuzz-" ++ name;
-    const fuzz_exe_path = try std.fs.path.join(b.allocator, &[_][]const u8{ b.cache_root, fuzz_executable_name });
+    const fuzz_exe_path = try std.fs.path.join(b.allocator, &.{ b.cache_root, fuzz_executable_name });
 
-    // We want `afl-clang-lto -o path/to/output path/to/object.o`
-    const fuzz_compile = b.addSystemCommand(&[_][]const u8{ "afl-clang-lto", "-o" });
-    // Add the output path to afl-clang-lto's args
-    fuzz_compile.addArg(fuzz_exe_path);
-    // Add the path to the object file to afl-clang-lto's args
-    fuzz_compile.addArtifactArg(fuzz_obj);
+    // We want `afl-clang-lto -o path/to/output path/to/library`
+    const fuzz_compile = b.addSystemCommand(&.{ "afl-clang-lto", "-o", fuzz_exe_path });
+    // Add the path to the library file to afl-clang-lto's args
+    fuzz_compile.addArtifactArg(fuzz_lib);
 
     // Install the cached output to the install 'bin' path
     const fuzz_install = b.addInstallBinFile(.{ .path = fuzz_exe_path }, fuzz_executable_name);
