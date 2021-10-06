db243761a2778193b4b8caddf293ad3b771fa813
Ryan Liptak <squeek502@hotmail.com>
2021-10-05 20:37:40 -0700

    sin: Handle NaN case and improve output for debugging

--- fuzzers/sin.zig
+++ fuzzers/sin.zig
@@ -13,13 +13,35 @@ pub fn zigMain() !void {
 
     // f32
     const float32 = @ptrCast(*const f32, buf[0..@sizeOf(f32)]).*;
+    std.debug.print("in : {b:0>32}\n", .{@bitCast(u32, float32)});
     const zig32 = std.math.sin(float32);
     const c32 = c.sinf(float32);
-    try std.testing.expectEqual(c32, zig32);
+    if (std.math.isNan(c32)) {
+        try std.testing.expect(std.math.isNan(zig32));
+    } else {
+        std.testing.expectEqual(c32, zig32) catch |err| {
+            std.debug.print("zig: {b:0>32}\nc  : {b:0>32}\n", .{
+                @bitCast(u32, zig32),
+                @bitCast(u32, c32),
+            });
+            return err;
+        };
+    }
 
     // f64
     var float64 = @ptrCast(*const f64, buf[0..]).*;
+    std.debug.print("in : {b:0>64}\n", .{@bitCast(u64, float64)});
     const zig64 = std.math.sin(float64);
     const c64 = c.sin(float64);
-    try std.testing.expectEqual(c64, zig64);
+    if (std.math.isNan(c64)) {
+        try std.testing.expect(std.math.isNan(zig64));
+    } else {
+        std.testing.expectEqual(c64, zig64) catch |err| {
+            std.debug.print("zig: {b:0>64}\nc  : {b:0>64}\n", .{
+                @bitCast(u64, zig64),
+                @bitCast(u64, c64),
+            });
+            return err;
+        };
+    }
 }
