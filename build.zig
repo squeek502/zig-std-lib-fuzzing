cffd4987d453ee856e4d15e8dde4e9de2d41c140
Ryan Liptak <squeek502@hotmail.com>
2023-05-02 01:05:33 -0700

    Make install step explicitly depend on compile step

--- build.zig
+++ build.zig
@@ -81,6 +81,7 @@ fn addFuzzer(b: *std.build.Builder, comptime name: []const u8, afl_clang_args: [
 
     // Install the cached output to the install 'bin' path
     const fuzz_install = b.addInstallBinFile(.{ .path = fuzz_exe_path }, fuzz_executable_name);
+    fuzz_install.step.dependOn(&fuzz_compile.step);
 
     // Add a top-level step that compiles and installs the fuzz executable
     const fuzz_compile_run = b.step("fuzz-" ++ name, "Build executable for fuzz testing '" ++ name ++ "' using afl-clang-lto");
