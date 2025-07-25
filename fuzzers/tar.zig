21bd8d91f4812ddaef675e6ab856602a2ce443e6
Ryan Liptak <squeek502@hotmail.com>
2025-07-25 16:12:03 -0700

    Update tar fuzzer

--- fuzzers/tar.zig
+++ fuzzers/tar.zig
@@ -5,7 +5,7 @@ export fn cMain() void {
 }
 
 comptime {
-    @export(cMain, .{ .name = "main", .linkage = .strong });
+    @export(&cMain, .{ .name = "main", .linkage = .strong });
 }
 
 pub fn main() !void {
@@ -16,25 +16,30 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     // Read the data from stdin
-    const stdin = std.io.getStdIn();
+    const stdin: std.fs.File = .stdin();
     const data = try stdin.readToEndAlloc(allocator, std.math.maxInt(usize));
     defer allocator.free(data);
-    var fbs = std.io.fixedBufferStream(data);
-    const reader = fbs.reader();
+    var fbs: std.io.Reader = .fixed(data);
 
+    var file_name_buffer: [std.fs.max_path_bytes]u8 = undefined;
+    var link_name_buffer: [std.fs.max_path_bytes]u8 = undefined;
     // Run tar parser
-    var tar = std.tar.iterator(reader, null);
+    var tar: std.tar.Iterator = .init(&fbs, .{
+        .file_name_buffer = &file_name_buffer,
+        .link_name_buffer = &link_name_buffer,
+    });
+    var null_writer_buf: [1]u8 = undefined;
+    var null_writer: std.io.Writer.Discarding = .init(&null_writer_buf);
     while (tar.next() catch null) |file| {
         switch (file.kind) {
             .directory => {},
-            .normal => {
-                file.write(std.io.null_writer) catch |err| {
+            .file => {
+                tar.streamRemaining(file, &null_writer.writer) catch |err| {
                     if (err == error.EndOfStream) break;
                     return err;
                 };
             },
-            .symbolic_link => {},
-            else => unreachable,
+            .sym_link => {},
         }
     }
 }
