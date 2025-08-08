2a9ea132655a942aa49f8eea7fa97e8cde10e180
Ryan Liptak <squeek502@hotmail.com>
2025-08-08 14:52:23 -0700

    deflate: Fix initial unused capacity not being big enough

--- fuzzers/deflate.zig
+++ fuzzers/deflate.zig
@@ -19,7 +19,7 @@ pub fn zigMain() !void {
     var decompress = std.compress.flate.Decompress.init(&fixed_reader, .raw, &.{});
 
     var aw: std.Io.Writer.Allocating = .init(allocator);
-    try aw.ensureUnusedCapacity(std.compress.flate.history_len);
+    try aw.ensureUnusedCapacity(std.compress.flate.max_window_len);
     defer aw.deinit();
 
     const decompressed_len = decompress.reader.streamRemaining(&aw.writer) catch {
